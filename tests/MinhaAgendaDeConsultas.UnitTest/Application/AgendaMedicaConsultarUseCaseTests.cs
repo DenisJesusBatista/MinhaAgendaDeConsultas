7d1f6b7469195652194ebb1ab6ed176f49815235
Alexandre Lustosa <lutosaalexandre@gmail.com>
2025-02-06 22:58:23 -0300

    test: aumenta cobertura de testes unitarios

---
+++ tests/MinhaAgendaDeConsultas.UnitTest/Application/AgendaMedicaConsultarUseCaseTests.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using MinhaAgendaDeConsultas.Domain.Repositorios.Agendamento;
+using MinhaAgendaDeConsultas.Domain.Repositorios;
+using MinhaAgendaDeConsultas.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using MinhaAgendaDeConsultas.Application.UseCases.AgendaMedica.Consultar;
+using AutoBogus;
+using MinhaAgendaDeConsultas.Domain.Entidades;
+using Bogus;
+using FluentAssertions;
+using MinhaAgendaDeConsultas.Communication.Resposta.Agendamento;
+using MinhaAgendaDeConsultas.Exceptions.ExceptionsBase;
+
+namespace MinhaAgendaDeConsultas.UnitTest.Application
+{
+    public class AgendaMedicaConsultarUseCaseTests
+    {
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IUnidadeDeTrabalho> _unidadeDeTrabalho;
+        private readonly Mock<IAgendaMedicaConsultaOnlyRepository> _agendaMedicaConsultaOnlyRepository;
+        private readonly Mock<IUsuarioReadOnlyRepositorio> _usuarioReadOnlyRepositorio;
+        private readonly AgendaMedicaConsultarUseCase _agendaMedicaConsultarUseCase;
+
+        public AgendaMedicaConsultarUseCaseTests()
+        {
+            _mapper = new();
+            _unidadeDeTrabalho = new();
+            _agendaMedicaConsultaOnlyRepository = new();
+            _usuarioReadOnlyRepositorio = new();
+            _agendaMedicaConsultarUseCase = new(_mapper.Object, _unidadeDeTrabalho.Object, _usuarioReadOnlyRepositorio.Object, _agendaMedicaConsultaOnlyRepository.Object);
+        }
+
+        [Fact]
+        public async Task Should_Return_IList_ResponseAgendaMedica_When_Email_Exists()
+        {
+            //Arrange
+            var usuario = new AutoFaker<Usuario>().Generate();
+
+            var appointments = new AutoFaker<AgendaMedica>().Generate(new Faker().Random.Int(1, 10));
+
+            _usuarioReadOnlyRepositorio.Setup(x => x.RecuperarPorEmail(It.IsAny<string>())).ReturnsAsync(usuario);
+
+            _agendaMedicaConsultaOnlyRepository
+                .Setup(x => x.ObterAgendasMedicias(It.IsAny<long>(),It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .ReturnsAsync(appointments);
+
+            //Act
+            var result = await _agendaMedicaConsultarUseCase.ObterAgendasMedicias(DateTime.Now, DateTime.Now, string.Empty);
+
+            //Assert
+            result.Should().BeOfType<List<ResponseAgendaMedica>>();
+            result.Should().NotBeNull();
+            result.Count.Should().Be(appointments.Count);
+        }
+
+        [Fact]
+        public async Task Should_Throw_When_Email_Not_Found()
+        {
+            //Arrange
+
+            //Act
+            var act = () => _agendaMedicaConsultarUseCase.ObterAgendasMedicias(DateTime.Now, DateTime.Now, string.Empty);
+
+            //Assert
+            await act.Should().ThrowAsync<Exception>().WithMessage("Médico não encontrado");
+        }
+
+        [Fact]
+        public async Task Should_Return_Empty_List_When_No_Appointments_Exist()
+        {
+            //Arrange
+            var usuario = new AutoFaker<Usuario>().Generate();
+
+            var appointments = new AutoFaker<AgendaMedica>().Generate(0);
+
+            _usuarioReadOnlyRepositorio.Setup(x => x.RecuperarPorEmail(It.IsAny<string>())).ReturnsAsync(usuario);
+
+            _agendaMedicaConsultaOnlyRepository
+                .Setup(x => x.ObterAgendasMedicias(It.IsAny<long>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .ReturnsAsync(appointments);
+
+            //Act
+            var result = await _agendaMedicaConsultarUseCase.ObterAgendasMedicias(DateTime.Now, DateTime.Now, string.Empty);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Count.Should().Be(appointments.Count);
+        }
+    }
+}
---
+++ tests/MinhaAgendaDeConsultas.UnitTest/Application/RegistrarMedicoUseCaseTests.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using MinhaAgendaDeConsultas.Domain.Repositorios.Medico;
+using MinhaAgendaDeConsultas.Domain.Repositorios;
+using MinhaAgendaDeConsultas.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinhaAgendaDeConsultas.Application.UseCases.Usuario.Registrar.Medico;
+using MinhaAgendaDeConsultas.Communication.Requisicoes.Medico;
+using AutoBogus;
+using MinhaAgendaDeConsultas.Domain.Entidades;
+using FluentAssertions;
+using MinhaAgendaDeConsultas.Exceptions.ExceptionsBase;
+
+namespace MinhaAgendaDeConsultas.UnitTest.Application
+{
+    public class RegistrarMedicoUseCaseTests
+    {
+        private readonly Mock<IMedicoReadOnlyRepositorio> _medicoReadOnlyRepositorio;
+        private readonly Mock<IMedicoWriteOnlyRepositorio> _medicoWriteOnlyRepositorio;
+        private readonly Mock<IUsuarioReadOnlyRepositorio> _usuarioReadOnlyRepositorio;
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IUnidadeDeTrabalho> _unidadeDeTrabalho;
+        private readonly RegistrarMedicoUseCase _registrarMedicoUseCase;
+
+        public RegistrarMedicoUseCaseTests()
+        {
+            _medicoReadOnlyRepositorio = new();
+            _medicoWriteOnlyRepositorio = new();
+            _usuarioReadOnlyRepositorio = new();
+            _mapper = new();
+            _unidadeDeTrabalho = new();
+            _registrarMedicoUseCase = new(_medicoReadOnlyRepositorio.Object, _mapper.Object, _unidadeDeTrabalho.Object, _medicoWriteOnlyRepositorio.Object, _usuarioReadOnlyRepositorio.Object);
+        }
+
+        [Fact]
+        public async Task Should_Return_ResponseRegistrarMedicoJson()
+        {
+            //Arrange
+            var request = new AutoFaker<RequisicaoRegistrarMedicoJson>().Generate();
+
+            var userRepositoryResponse = new AutoFaker<Usuario>().Generate();
+
+            var mapperResult = new AutoFaker<Medico>().RuleFor(x => x.Nome, request.Nome).Generate();
+
+            _medicoReadOnlyRepositorio.Setup(x => x.ExisteMedicoComCpf(It.IsAny<string>())).ReturnsAsync(false);
+            _medicoReadOnlyRepositorio.Setup(x => x.ExisteMedicoComCrm(It.IsAny<string>())).ReturnsAsync(false);
+            _medicoReadOnlyRepositorio.Setup(x => x.ExisteMedicoUsuarioComEmail(It.IsAny<string>())).ReturnsAsync(true);
+
+            _usuarioReadOnlyRepositorio.Setup(x => x.RecuperarPorEmail(It.IsAny<string>())).ReturnsAsync(userRepositoryResponse);
+
+            _mapper.Setup(x => x.Map<Medico>(It.IsAny<RequisicaoRegistrarMedicoJson>())).Returns(mapperResult);
+
+            //Act
+            var result = await _registrarMedicoUseCase.Executar(request);
+
+            //Assert
+            result.Nome.Should().Be(request.Nome);
+        }
+
+        [Fact]
+        public async Task Should_Throw_When_Request_Cpf_Is_Null()
+        {
+            //Arrange
+            var request = new AutoFaker<RequisicaoRegistrarMedicoJson>().RuleFor(x => x.Cpf, string.Empty).Generate();
+
+            //Act
+            var act = () => _registrarMedicoUseCase.Executar(request);
+
+            //Assert
+            await act.Should().ThrowAsync<ErrosDeValidacaoException>();
+        }
+
+        [Fact]
+        public async Task Should_Throw_When_Request_Crm_Is_Null()
+        {
+            //Arrange
+            var request = new AutoFaker<RequisicaoRegistrarMedicoJson>().RuleFor(x => x.Crm, string.Empty).Generate();
+
+            //Act
+            var act = () => _registrarMedicoUseCase.Executar(request);
+
+            //Assert
+            await act.Should().ThrowAsync<ErrosDeValidacaoException>();
+        }
+
+        [Fact]
+        public async Task Should_Throw_When_Request_Nome_Is_Null()
+        {
+            //Arrange
+            var request = new AutoFaker<RequisicaoRegistrarMedicoJson>().RuleFor(x => x.Nome, string.Empty).Generate();
+
+            //Act
+            var act = () => _registrarMedicoUseCase.Executar(request);
+
+            //Assert
+            await act.Should().ThrowAsync<ErrosDeValidacaoException>();
+        }
+    }
+}
